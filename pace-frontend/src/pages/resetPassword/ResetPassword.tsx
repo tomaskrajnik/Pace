16d3df57510d0c0439f6f9288303dfd41a8ae337
Tomas Krajnik <tomas.krajnik@gmail.com>
2021-11-11 09:36:15 +0100

    removing header

--- pace-frontend/src/pages/resetPassword/ResetPassword.tsx
+++ pace-frontend/src/pages/resetPassword/ResetPassword.tsx
@@ -47,11 +47,6 @@ const ResetPassword: React.FC = () => {
                 <div className="max-w-md w-full">
                     <img className="mx-auto h-5 w-auto" src={paceLogo} alt="Pace Logo" />
                     <div className="h-100 py-28">
-                        <div>
-                            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                                Forgot your password?
-                            </h2>
-                        </div>
                         <form className="mt-8 space-y-6" action="#" method="POST">
                             <NormalText>We will send a recovery link to:</NormalText>
                             <div className="rounded-md shadow-sm -space-y-px">
